7de823fe420c1bd0705470f9db015e09c84e00cb
Nobuyuki Umetani <n.umetani@gmail.com>
2020-11-13 17:24:16 +0900

    script to install delfem2 into blender python

--- scripts/macos_blender.sh
+++ scripts/macos_blender.sh
@@ -16,11 +16,18 @@ version_python=$(cut -d' ' -f 2 <<< ${version_python})
 version_python=${version_python%.*}
 echo "version python: ${version_python}"
 
+brew install "python@${version_python}"
+path_include_from=/usr/local/opt/python@${version_python}/Frameworks/Python.framework/Headers
+path_include_to=/Applications/Blender.app/Contents/Resources/${version_blender}/python/include/python${version_python}m
+
+echo ${path_include_from}
+echo ${path_include_to}
+cp -r ${path_include_from}/*.h ${path_include_to}
+cp -r ${path_include_from}/internal ${path_include_to}
+
 ${path_python} -m ensurepip     # make sure pip is installed
 ${path_python} -m pip install --upgrade pip # upgrade pip
 
-# one needs to copy original python header files to blender's python's include directory
-# see https://qiita.com/syoyo/items/ed9a9678a707f1578d74 for the detail 
 ${path_python} -m pip install -e . # install delfem2
 
 
